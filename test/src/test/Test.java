7e8a10082b27a7976be3c0a0892d78cd8bc15ae3
motherfather <mother@father>
2017-03-17 18:39:04 +0900

    all

---
+++ test/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num; j++) {
+				
+			}
+		}
+	}
+}
